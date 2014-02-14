7f6a35b9ed96a5d36b88507f1b12213a7fe9cb32
wolambi <lambi.dwm@gmail.com>
2014-02-14 16:04:49 +0100

    ladt Wolambi

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ $(function(){
 
   var contentmove = $(".contentmove");
 
-
+	$(".projetsClose").click(function(e) {
+		$this = $(this);
+		var theLi = $this.parent("li");
+		theLi.css("height","324px");// PAS propre a modifier ...
+	});
 
 
 	$(".loginSubmit").click(function(e) {
--- pages/projet.php
+++ pages/projet.php
@@ -2,14 +2,42 @@
 <div id="projets" class="displayNone">
 	<ul>
 		<li>
-			<div class="jours">
-				<span><span class="nbr_jours">3</span>j</span>
+			<div class="projetsClose">
+				<div class="jours">
+					<span><span class="nbr_jours">3</span>j</span>
+				</div>
+				<div class="projetContent">
+					<h1>Flowchart Life</h1>
+					<div class="projetProf">DIEU</div>
+					<div class="projetCreat">Antonin lambinet</div>
+				</div>
 			</div>
-			<div class="projetContent">
-				<h1>Flowchart Life</h1>
-				<div class="projetProf">DIEU</div>
-				<div class="projetCreat">Antonin lambinet</div>
-			</div>
+		<div class="projetsOpen">
+			<ul>
+				<li>
+					<span class="projetJoursTache" ><span class="nbr_jours_tache">1</span>j</span>
+					<p>Cohérence dsfsdfdsfgd dsgnxvn nfxb ioxfnv cofn cjngvc, ogne dfngld dng ng ijb dingoidhgo  oigndskfns  à revoir.</p>
+				</li>
+				<li>
+					<span class="projetJoursTache" ><span class="nbr_jours_tache">1</span>j</span>
+					<p>Cohérence graphique à revoir.</p>
+				</li>
+			</ul>
+			<nav>
+<!-- 				<ul>
+					<li>
+						<div>
+							<span></span>
+							<span></span>
+						</div>
+					</li>
+
+					<li></li>
+				</ul> -->
+			</nav>
+		</div>
+
+
 		</li>
 		<li>
 			<div class="jours">
@@ -51,89 +79,5 @@
 				<div class="projetCreat">BipBoupsBomm</div>
 			</div>
 		</li>
-
-		<li>
-			<div class="jours">
-				<span><span class="nbr_jours">3</span>j</span>
-			</div>
-			<div class="projetContent">
-				<h1>Flowchart Life</h1>
-				<div class="projetProf">DIEU</div>
-				<div class="projetCreat">Antonin lambinet</div>
-			</div>
-		</li>
-		<li>
-			<div class="jours">
-				<span><span class="nbr_jours">5</span>j</span>
-			</div>
-			<div class="projetContent">
-				<h1>Luvudi</h1>
-				<div class="projetProf">Gegory</div>
-				<div class="projetCreat">Antoine El Gringo</div>
-			</div>
-		</li>
-		<li>
-			<div class="jours">
-				<span><span class="nbr_jours">12</span>j</span>
-			</div>
-			<div class="projetContent">
-				<h1>Flowchart Life</h1>
-				<div class="projetProf">kirikou</div>
-				<div class="projetCreat">Francois Dieudonner</div>
-			</div>
-		</li>
-		<li>
-			<div class="jours">
-				<span><span class="nbr_jours">99</span>j</span>
-			</div>
-			<div class="projetContent">
-				<h1>Flowchart Life</h1>
-				<div class="projetProf">kiki</div>
-				<div class="projetCreat">A Rat de la Touff</div>
-			</div>
-		</li>
-		<li>
-			<div class="jours">
-				<span><span class="nbr_jours">183</span>j</span>
-			</div>
-			<div class="projetContent">
-				<h1>Flowchart Life</h1>
-				<div class="projetProf">Boble p'tit matelo</div>
-				<div class="projetCreat">BipBoupsBomm</div>
-			</div>
-		</li>
-
-		<li>
-			<div class="jours">
-				<span><span class="nbr_jours">3</span>j</span>
-			</div>
-			<div class="projetContent">
-				<h1>Flowchart Life</h1>
-				<div class="projetProf">DIEU</div>
-				<div class="projetCreat">Antonin lambinet</div>
-			</div>
-		</li>
-		<li>
-			<div class="jours">
-				<span><span class="nbr_jours">5</span>j</span>
-			</div>
-			<div class="projetContent">
-				<h1>Luvudi</h1>
-				<div class="projetProf">Gegory</div>
-				<div class="projetCreat">Antoine El Gringo</div>
-			</div>
-		</li>
-		<li>
-			<div class="jours">
-				<span><span class="nbr_jours">12</span>j</span>
-			</div>
-			<div class="projetContent">
-				<h1>Flowchart Life</h1>
-				<div class="projetProf">kirikou</div>
-				<div class="projetCreat">Francois Dieudonner</div>
-			</div>
-		</li>
-
-
 	</ul>
 </div><!-- End projets -->
