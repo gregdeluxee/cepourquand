f8fb864c2ac77591e406ee24dda5aad5a4eba478
wolambi <lambi.dwm@gmail.com>
2014-02-20 17:39:14 +0100

    chat OK

--- pages/chat.php
+++ pages/chat.php
@@ -21,7 +21,29 @@
 					<span>Moi</span>
 				</div>
 				<div class="contentChatContenuEtDate">
-					<p>C’est quoi ce briefing de merde j’y comprends rien ! Minnie était censé m’expliquer mais elle préfère se faire retourner par Donald ! Help meeeeee</p>
+					<p>C’esieorgodirj rgoidjfigojdoifgjodijfgoidjfg à!djiofbàçdj oijsàçdjb dfbjd àçgjsoin sdofnsd oerjgfdkn goie fozen sdoferg eogerkg Minnie était censé m’expliquer mais elle préfère se faire retourner par Donald ! Help meeeeee</p>
+					<span> 14 Fevrier 25:62</span>
+
+				</div>
+			</li>
+			<li>
+				<div class="nomEtImgage">
+					<img src="img/profil_image.png" alt="moi">
+					<span>Moi</span>
+				</div>
+				<div class="contentChatContenuEtDate">
+					<p>VIVE la biere</p>
+					<span> 14 Fevrier 25:62</span>
+
+				</div>
+			</li>
+			<li>
+				<div class="nomEtImgage">
+					<img src="img/profil_image.png" alt="moi">
+					<span>Moi</span>
+				</div>
+				<div class="contentChatContenuEtDate">
+					<p>VIVE la BOOM</p>
 					<span> 14 Fevrier 25:62</span>
 
 				</div>
